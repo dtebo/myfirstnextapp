a949cd71add4e18f96f311ec10e2566a626a3fee
dtebo <darrentebo83@outlook.com>
2021-05-26 18:19:50 -0400

    added nextjs feature list

--- pages/about.js
+++ pages/about.js
@@ -11,7 +11,15 @@ const About = () => {
                 <h1>About FirstNextJS</h1>
                 <div className={aboutStyles.description}>
                     <p>
-                        This app is designed to demonstrate the capabilities of NextJS.
+                        This app is designed to demonstrate the incredible capabilities of NextJS.
+                        These capabilities include: <br />
+                        <ul>
+                            <li>Simplified Routing</li>
+                            <li>Faster load times</li>
+                            <li>Heavy Image Optimization</li>
+                            <li>Built-in CSS Support</li>
+                            <li>Code-splitting and Bundling</li>
+                        </ul>
                     </p>
                 </div>
             </section>
