fa09e3d297be6c5794bf3f46ea45a7bb902a6121
dtebo <darrentebo83@outlook.com>
2021-05-26 17:26:44 -0400

    initial commit

---
+++ components/Intro.js
@@ -0,0 +1,23 @@
+import styles from '../styles/home.module.css'
+import introStyles from '../styles/Intro.module.css'
+
+import Link from 'next/link'
+
+const Intro = () => {
+    return (
+        <>
+            <section className={styles.container}>
+                <h1>My First NextJS App!</h1>
+                <p>Welcome to my first next app. I hope you like what you see!</p>
+                <p>
+                    Please visit my favorite news site, Read{'\n'}
+                    <Link href='https://www.neowin.net'>
+                        <a className={introStyles.link}>Neowin</a>
+                    </Link>
+                </p>
+            </section>
+        </>
+    )
+}
+
+export default Intro
