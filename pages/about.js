35a0e184219c59a2e170ca13b5d89a5e32de3ee9
dtebo <darrentebo83@outlook.com>
2021-05-26 18:00:10 -0400

    updated about page title

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,11 @@
+import Head from 'next/head'
+
 const About = () => {
     return(
         <>
+            <Head>
+                <title>My First NextJS App - About</title>
+            </Head>
             <h1>About FirstNextJS</h1>
         </>
     )
